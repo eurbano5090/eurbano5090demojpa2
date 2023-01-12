6170e5beea3f0635d54161e1d66d730556037505
elisanoelia <enurbanom@outlook.com>
2023-01-11 23:41:07 -0300

    cambio swagger

--- src/main/java/com/openbootcamp/demojpa/configurations/SwaggerConfig.java
+++ src/main/java/com/openbootcamp/demojpa/configurations/SwaggerConfig.java
@@ -1,38 +1,32 @@
 package com.openbootcamp.demojpa.configurations;
 
-/*import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig {
+@EnableSwagger2
+public class SwaggerConfig{
+
+	
+	
 	   @Bean
 	    public Docket api(){
-
-	        return new Docket(DocumentationType.SWAGGER_2)
-	                .apiInfo(apiDetails())
+          return new Docket(DocumentationType.SWAGGER_2)
 	                .select()
-	                .apis(RequestHandlerSelectors.any())
+	                .apis(RequestHandlerSelectors.basePackage("com.openbootcamp.demojpa.controllers"))
 	                .paths(PathSelectors.any())
-	                .build();
-	    }
+	                .build()
+                    .apiInfo(new ApiInfoBuilder().title("Api info usuario").description("my api rest swagger")
+                    .build());
+	        }
+	             
 
-	    private ApiInfo apiDetails(){
-	        return new ApiInfo("Spring Boot Book API REST",
-	                "Library Api rest docs",
-	                "1.0",
-	                "http://www.google.com",
-	                new Contact("Alan", "http://www.google.com", "alan@example.com"),
-	                "MIT",
-	                "http://www.google.com",
-	                Collections.emptyList());
-	    }
-}*/
+	     
+}
--- src/main/java/com/openbootcamp/demojpa/controllers/LaptopController.java
+++ src/main/java/com/openbootcamp/demojpa/controllers/LaptopController.java
@@ -3,6 +3,8 @@ package com.openbootcamp.demojpa.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.ws.rs.Produces;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,80 +14,83 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.openbootcamp.demojpa.models.Laptop;
 import com.openbootcamp.demojpa.repositories.LaptopRepository;
 
 @RestController
+@RequestMapping("/")
 public class LaptopController {
 
-	private final Logger log=LoggerFactory.getLogger(LaptopController.class);
-	
+	private final Logger log = LoggerFactory.getLogger(LaptopController.class);
+
 	private LaptopRepository laptoprepository;
 
 	public LaptopController(LaptopRepository laptoprepository) {
 		super();
 		this.laptoprepository = laptoprepository;
 	}
-	
+
 	@GetMapping("/api/laptops")
-	public List<Laptop>finAll(){
+	public List<Laptop> finAll() {
 		return laptoprepository.findAll();
-		
+
 	}
+
 	@GetMapping("/api/laptops/{id}")
 	public ResponseEntity<Laptop> findById(@PathVariable Long id) {
 		Optional<Laptop> oplaptop = laptoprepository.findById(id);
-	// return opbook.orElse(null);
-		if(oplaptop.isPresent()) 
+		// return opbook.orElse(null);
+		if (oplaptop.isPresent())
 			return ResponseEntity.ok(oplaptop.get());
-		else 
+		else
 			return ResponseEntity.notFound().build();
-		}
-	//	return opbook.map(ResponseEntity:ok).orElse(()->ResponseEntity.notFound().build());
-	
+	}
+	// return
+	// opbook.map(ResponseEntity:ok).orElse(()->ResponseEntity.notFound().build());
+
 	@PostMapping("/api/laptops")
 	public Laptop crear(@RequestBody Laptop laptop) {
 		return laptoprepository.save(laptop);
-		
+
 	}
-		@PutMapping("/api/laptops")
-		public ResponseEntity<Laptop> update(@RequestBody Laptop laptop) {
-			
-			if (laptop.getId()!=null) {
-				log.warn("trying to update a non existent book ");
-				return ResponseEntity.badRequest().build();
-			}
-			if(!laptoprepository.existsById(laptop.getId())) {
-				log.warn("trying to update a non existent book ");
-				return ResponseEntity.notFound().build();
-			}
-			Laptop result=laptoprepository.save(laptop);
-			return ResponseEntity.ok(result);
-			
-			
-		}
-		//@ApiIgnore
-		@DeleteMapping("/api/laptops/{id}")
-		public ResponseEntity<Laptop>delete(@PathVariable Long id){
-			if(!laptoprepository.existsById(id)) {
-				log.warn("trying to delete a non existent book ");
-				return ResponseEntity.notFound().build();
-				}
-			laptoprepository.deleteById(id);
-			return ResponseEntity.noContent().build();
-			
-		}
-		//@ApiIgnore
-		@DeleteMapping("/api/laptops")
-		public ResponseEntity<Laptop>deleteAll(){
-			log.info("Rest request for deleting all books");
-			laptoprepository.deleteAll();
-			return ResponseEntity.noContent().build();
-			
-		}
-		}
 
+	@PutMapping("/api/laptops")
+	public ResponseEntity<Laptop> update(@RequestBody Laptop laptop) {
 
+		if (laptop.getId() != null) {
+			log.warn("trying to update a non existent laptops ");
+			return ResponseEntity.badRequest().build();
+		}
+		if (!laptoprepository.existsById(laptop.getId())) {
+			log.warn("trying to update a non existent laptops ");
+			return ResponseEntity.notFound().build();
+		}
+		Laptop result = laptoprepository.save(laptop);
+		return ResponseEntity.ok(result);
+
+	}
+
+	// @ApiIgnore
+	@DeleteMapping("/api/laptops/{id}")
+	public ResponseEntity<Laptop> delete(@PathVariable Long id) {
+		if (!laptoprepository.existsById(id)) {
+			log.warn("trying to delete a non existent laptops ");
+			return ResponseEntity.notFound().build();
+		}
+		laptoprepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	// @ApiIgnore
+	@DeleteMapping("/api/laptops")
+	public ResponseEntity<Laptop> deleteAll() {
+		log.info("Rest request for deleting all laptops");
+		laptoprepository.deleteAll();
+		return ResponseEntity.noContent().build();
+
+	}
+}
